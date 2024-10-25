0ba04f0fa12d1f5c105d41816fc283d1e0bd0911
Richard Gomez <rmgomez368@gmail.com>
2024-10-25 11:45:19 -0400

    feat(generic): match special characters

--- cmd/generate/config/rules/generic.go
+++ cmd/generate/config/rules/generic.go
@@ -20,7 +20,7 @@ func GenericCredential() *config.Rule {
 			"password",
 			"auth",
 			"access",
-		}, `[0-9a-z\-_.=]{10,150}`, true),
+		}, `[\w!@#$%^&*.=-]{10,150}`, true),
 		Keywords: []string{
 			"key",
 			"api",
@@ -46,6 +46,7 @@ func GenericCredential() *config.Rule {
 		utils.GenerateSampleSecret("generic", "Zf3D0LXCM3EIMbgJpUNnkRtOfOueHznB"),
 		`"client_id" : "0afae57f3ccfd9d7f5767067bc48b30f719e271ba470488056e37ab35d4b6506"`,
 		`"client_secret" : "6da89121079f83b2eb6acccf8219ea982c3d79bccc3e9c6a85856480661f8fde",`,
+		`SECRET_KEY = 'django-insecure-#0p-_n8*huf#ieh2@!mlf3hqyb^#tr!*8=g51*4'`, // gitleaks:allow
 	}
 	fps := []string{
 		`client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.client-vpn-endpoint.id`,
